2e9914450773747ee2f4f34dbc9b3c473bed8878
impronez <orzaev7312@gmail.com>
2025-04-09 14:47:45 +0300

    Поправил типы выходных переменных

--- Accomodations/Accommodations/Dto/BookingDto.cs
+++ Accomodations/Accommodations/Dto/BookingDto.cs
@@ -8,24 +8,24 @@ public class BookingDto
         string endDateString,
         string currencyString)
     {
-        var isCorrectId = int.TryParse(userIdString, out int id);
+        var isCorrectId = int.TryParse(userIdString, out var id);
         if (!isCorrectId)
-            throw new ArgumentException("Invalid user id");
+            throw new ArgumentException($"Invalid user id: '{userIdString}. User id must be an integer'");
         
-        var isCorrectStartDate = DateTime.TryParse(startDateString, out DateTime startDate);
+        var isCorrectStartDate = DateTime.TryParse(startDateString, out var startDate);
         if (!isCorrectStartDate)
-            throw new ArgumentException("Invalid start date");
+            throw new ArgumentException($"Invalid start date: '{startDateString}. Date format: 'MM/DD/YYYY'");
         
-        var isCorrectEndDate = DateTime.TryParse(endDateString, out DateTime endDate);
+        var isCorrectEndDate = DateTime.TryParse(endDateString, out var endDate);
         if (!isCorrectEndDate)
-            throw new ArgumentException("Invalid end date");
+            throw new ArgumentException($"Invalid end date: '{endDateString}. Date format: 'MM/DD/YYYY'");
         
         if (string.IsNullOrWhiteSpace(category))
             throw new ArgumentException("Empty category");
-        
-        var isCorrectCurrency = Enum.TryParse(currencyString, out CurrencyDto currency);
-        if (!isCorrectCurrency)
-            throw new ArgumentException("Invalid currency");
+
+        var isCorrectCurrency = Enum.TryParse(currencyString, true, out CurrencyDto currency);
+         if (!isCorrectCurrency)
+             throw new ArgumentException("Invalid currency");
 
         return new BookingDto
         {
