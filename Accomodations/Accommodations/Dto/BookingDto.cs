d9996c4f74be144cf9b3bf0b2ac15827528577fd
impronez <orzaev7312@gmail.com>
2025-04-09 14:51:34 +0300

    Поправил форматирование

--- Accomodations/Accommodations/Dto/BookingDto.cs
+++ Accomodations/Accommodations/Dto/BookingDto.cs
@@ -11,21 +11,21 @@ public class BookingDto
         var isCorrectId = int.TryParse(userIdString, out var id);
         if (!isCorrectId)
             throw new ArgumentException($"Invalid user id: '{userIdString}. User id must be an integer'");
-        
+
         var isCorrectStartDate = DateTime.TryParse(startDateString, out var startDate);
         if (!isCorrectStartDate)
             throw new ArgumentException($"Invalid start date: '{startDateString}. Date format: 'MM/DD/YYYY'");
-        
+
         var isCorrectEndDate = DateTime.TryParse(endDateString, out var endDate);
         if (!isCorrectEndDate)
             throw new ArgumentException($"Invalid end date: '{endDateString}. Date format: 'MM/DD/YYYY'");
-        
+
         if (string.IsNullOrWhiteSpace(category))
             throw new ArgumentException("Empty category");
 
         var isCorrectCurrency = Enum.TryParse(currencyString, true, out CurrencyDto currency);
-         if (!isCorrectCurrency)
-             throw new ArgumentException("Invalid currency");
+        if (!isCorrectCurrency)
+            throw new ArgumentException("Invalid currency");
 
         return new BookingDto
         {
@@ -36,7 +36,7 @@ public class BookingDto
             Currency = currency
         };
     }
-    
+
     public int UserId { get; private init; }
     public DateTime StartDate { get; private init; }
     public DateTime EndDate { get; private init; }
