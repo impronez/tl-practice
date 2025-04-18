9aabc7c094a1048dd7909496ff2fa4f31283eb18
impronez <orzaev7312@gmail.com>
2025-04-18 10:02:39 +0300

    Поправки

--- OrderManager/Program.cs
+++ OrderManager/Program.cs
@@ -1,22 +1,18 @@
 ﻿PrintWelcomeMessage();
 
-string productName;
-uint productQuantity;
-string userName;
-string deliveryAddress;
-
 while (true)
 {
-    productName = GetStringParameterFromConsole("название товара");
-    productQuantity = GetUintParameterFromConsole("количество товара");
-    userName = GetStringParameterFromConsole("имя");
-    deliveryAddress = GetStringParameterFromConsole("адрес доставки");
+    string productName = GetRequiredStringParameterFromConsole("название товара");
+    uint productQuantity = GetRequiredPositiveUintParameterFromConsole("количество товара");
+    string userName = GetRequiredStringParameterFromConsole("имя");
+    string deliveryAddress = GetRequiredStringParameterFromConsole("адрес доставки");
 
-    if (IsCorrectOrderData(productName, productQuantity, userName, deliveryAddress))
+    if (IsOrderConfirmed(productName, productQuantity, userName, deliveryAddress))
+    {
+        PrintSuccessfulOrderConfirmationMessage(productName, productQuantity, userName, deliveryAddress);
         break;
+    }
 }
-
-PrintSuccessfulOrderConfirmationMessage(productName, productQuantity, userName, deliveryAddress);
 
 static void PrintSuccessfulOrderConfirmationMessage(string productName,
     uint productQuantity,
@@ -27,7 +23,7 @@ static void PrintSuccessfulOrderConfirmationMessage(string productName,
         $"{userName}! Ваш заказ '{productName}' в количестве {productQuantity} шт. оформлен! Доставка по адресу: {deliveryAddress}");
 }
 
-static bool IsCorrectOrderData(string productName,
+static bool IsOrderConfirmed(string productName,
     uint productQuantity,
     string userName,
     string deliveryAddress)
@@ -43,6 +39,22 @@ static bool IsCorrectOrderData(string productName,
     return isConfirmed;
 }
 
+static uint GetRequiredPositiveUintParameterFromConsole(string parameterName)
+{
+    uint value;
+
+    do
+    {
+        value = GetUintParameterFromConsole(parameterName);
+        if (value == 0)
+        {
+            Console.WriteLine($"Значение '{parameterName}' не может быть равно 0!");
+        }
+    } while (value == 0);
+
+    return value;
+}
+
 static uint GetUintParameterFromConsole(string parameterName)
 {
     uint value;
@@ -52,7 +64,7 @@ static uint GetUintParameterFromConsole(string parameterName)
     while (true)
     {
         var isParsed = uint.TryParse(Console.ReadLine(), out value);
-        if (!isParsed || value == 0)
+        if (!isParsed)
             Console.Write($"Невалидное значение! Введите {parameterName} еще раз: ");
         else
             break;
@@ -61,22 +73,25 @@ static uint GetUintParameterFromConsole(string parameterName)
     return value;
 }
 
-static string GetStringParameterFromConsole(string parameterName)
+static string GetRequiredStringParameterFromConsole(string parameterName)
 {
-    string value;
-
-    Console.Write($"Введите {parameterName}: ");
-
     while (true)
     {
-        value = Console.ReadLine();
-        if (!string.IsNullOrWhiteSpace(value))
-            break;
+        string? value = GetStringParameterFromConsole(parameterName);
+        if (!string.IsNullOrEmpty(value))
+        {
+            return value;
+        }
 
-        Console.Write($"Введенное значение не может быть пустым. Введите {parameterName} еще раз: ");
+        Console.WriteLine("Введенное значение не может быть пустым!");
     }
+}
 
-    return value;
+static string? GetStringParameterFromConsole(string parameterName)
+{
+    Console.Write($"Введите {parameterName}: ");
+
+    return Console.ReadLine();
 }
 
 static void PrintWelcomeMessage()
