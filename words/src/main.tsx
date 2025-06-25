bdcaf7a1a30067b1962dca0f9f2044ec9bab23e1
impronez <orzaev7312@gmail.com>
2025-06-25 09:46:57 +0300

    Добавил StyledEngineProvider

--- words/src/main.tsx
+++ words/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { StyledEngineProvider } from "@mui/material";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <StyledEngineProvider injectFirst>
+        <App />
+      </StyledEngineProvider>
     </BrowserRouter>
   </StrictMode>
 );
